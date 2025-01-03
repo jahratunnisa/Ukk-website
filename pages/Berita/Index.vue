215b3a9c22a6f36f9e665100e8d2766fd4b44fcd
jahratunnisa <141713515+jahratunnisa@users.noreply.github.com>
2025-01-03 15:11:21 +0700

    Add files via upload

---
+++ pages/Berita/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="card" style="width: 18rem;">
+  <img src="/assets/Guru.jpeg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Amanat</h5>
+    <p class="card-text">Pengibaran bendera merahputih hari senin.</p>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem;">
+  <img src="/assets/kami.jpeg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Porak</h5>
+    <p class="card-text">Pekan olahraga antar kelas</p>
+  </div>
+</div>
+  </template>
